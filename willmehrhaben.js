463c9ef0d49c403a5f8d1768c5b5348557371f85
g <g@oknaj.eu>
2023-05-29 08:55:44 +0200

    use html tag attributes as trigger

--- willmehrhaben.js
+++ willmehrhaben.js
@@ -6,21 +6,47 @@ function isAdPage() {
 function checkUrlId(willhabenId) {
     const url = window.location.toString();
     const v = url.split("-");
-    const idFromUrl = v[v.length-1];
-    return idFromUrl == willhabenId;
+    const idFromUrl = v[v.length-1].split("/")[0];
+    if (idFromUrl === willhabenId) {
+        return true;
+    } else {
+        console.warn("[willmehrhaben] JSON data ID (" + willhabenId + ") does not match the id from the URL (" + idFromUrl + ")");
+        return false;
+    }
 }
 
-function insertReloadButton(editDateTag) {
+function getEmptyPlaceholder() {
+    const editDateTag = document.querySelector('[data-testid="ad-detail-ad-edit-date-top"]');
+
+    let publishedDateSpan = document.getElementById("WMHPublishedDate");
+
+    if (!publishedDateSpan) {
+        publishedDateSpan = document.createElement("span")
+        publishedDateSpan.id = "WMHPublishedDate";
+        editDateTag.after(publishedDateSpan);
+    } else {
+        publishedDateSpan.replaceChildren();
+    }
+
+    return publishedDateSpan;
+}
+
+function insertReloadButton(str) {
     const appendString = " | Veröffentlicht: ";
     const button = document.createElement("a");
 
-    button.textContent = "⟳";
-    button.title = "neu laden";
-    button.addEventListener('click', location.reload.bind(location));
-    button.style = "cursor:pointer;";
+    const publishedDateSpan = getEmptyPlaceholder();
 
-    editDateTag.textContent = editDateTag.textContent + appendString;
-    editDateTag.after(button);
+    if (str) {
+        publishedDateSpan.replaceChildren(str);
+    } else {
+        button.textContent = "⟳";
+        button.title = "neu laden";
+        button.addEventListener('click', location.reload.bind(location));
+        button.style = "cursor:pointer;";
+
+        publishedDateSpan.replaceChildren(appendString, button);
+    }
 }
 
 function injectPublishedDate() {
@@ -31,8 +57,6 @@ function injectPublishedDate() {
     const dataTag = document.getElementById("__NEXT_DATA__");
 
     let appendString = "";
-
-    const editDateTag = document.querySelector('[data-testid="ad-detail-ad-edit-date-top"]'); 
 
     if (!dataTag) {
         console.warn("[willmehrhaben] could not find NEXT_DATA tag");
@@ -45,37 +69,35 @@ function injectPublishedDate() {
 
         if (!pageProps["advertDetails"]) {
             console.warn("[willmehrhaben] could not find 'advertDetails' property");
-            insertReloadButton(editDateTag);
+            insertReloadButton();
         } else {
             if (checkUrlId(pageProps["advertDetails"]["id"])) {
                 const firstPublished = pageProps["advertDetails"]["firstPublishedDate"];
                 const formattedPublishedDate = firstPublished.slice(8,10) + "." + firstPublished.slice(5,7) + "." + firstPublished.slice(0,4) + ", " + firstPublished.slice(11,13) + ":" + firstPublished.slice(14,16) + " Uhr";
                 appendString = " | Veröffentlicht: " + formattedPublishedDate;
+                insertReloadButton(appendString);
             } else {
-                console.warn("[willmehrhaben] JSON data id does not match the id from the URL");
-                insertReloadButton(editDateTag);
+                console.warn("[willmehrhaben] JSON data ID (" + pageProps["advertDetails"]["id"] + ") does not match the id from the URL");
+                insertReloadButton();
             }
         }
     }
 
-    setTimeout(() => {
-        editDateTag.textContent = editDateTag.textContent + appendString;
-        console.debug("[willmehrhaben] done");
-    }, 1000);
+    console.debug("[willmehrhaben] done");
 }
 
 const attrObserver = new MutationObserver((mutations) => {
   mutations.forEach(mu => {
-    if (mu.type !== "attributes" && mu.attributeName !== "class") return;
-    console.debug ("[willmehrhaben] mutation of html class!");
+    if (mu.type !== "attributes" && !(mu.attributeName === "class" && mu.attributeName === "data-lt-installed")) return;
+    console.debug ("[willmehrhaben] mutation of html tag");
     const iP = isAdPage();
-    if (!iP["result"]) { 
+    if (!iP["result"]) {
         console.debug("not an ad page: " + iP["url"]);
-        return; 
+        return;
     }
     console.debug("[willmehrhaben] on an ad page");
-    if (mu.target.className == " ") {
-        console.debug("[willmehrhaben] html has finished loading");
+    if ((mu.attributeName === "class" && mu.target.className === " ") || (mu.attributeName === "data-lt-installed" && mu.target.attributes["data-lt-installed"].value === "true")) {
+        console.debug("[willmehrhaben] html has finished loading (" + mu.attributeName + ")");
         injectPublishedDate()
     } else {
         console.debug("[willmehrhaben] html is still loading");
@@ -85,5 +107,3 @@ const attrObserver = new MutationObserver((mutations) => {
 
 const htmlTag = document.documentElement;
 attrObserver.observe(htmlTag, {attributes: true});
-
-injectPublishedDate();
