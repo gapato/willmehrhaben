1ddf4f91cc293c5d801a262a1115c12fe7cde8b0
g <g@oknaj.eu>
2023-06-14 12:57:50 +0200

    get JSON data from upstream instead of relying on embedded data

--- willmehrhaben.js
+++ willmehrhaben.js
@@ -5,16 +5,24 @@ let NEXT_DATA = null;
 const hide_old_text = "Alte Anzeigen ausblenden";
 const show_old_text = "Alte Anzeigen einblenden";
 
+function tryRemove(id) {
+    const elem = document.getElementById(id);
+    if (elem !== null) {
+        console.debug("[willmehrhaben] removing element " + id);
+        elem.remove();
+    }
+}
+
 function checkRealEstate() {
     // checking the URL is not enough (search agents)
 
-    const pageProps = getPageProps();
+    return getPageProps().then((pageProps) => {
+        const details = pageProps["advertDetails"] || pageProps["searchResult"];
 
-    const details = pageProps["advertDetails"] || pageProps["searchResult"];
+        if (typeof details === "undefined") return false;
 
-    if (typeof details === "undefined") return false;
-
-    return details["breadcrumbs"][1]["displayName"] === "Immobilien";
+        return details["breadcrumbs"][1]["displayName"] === "Immobilien";
+    });
 }
 
 function isAdPage() {
@@ -25,16 +33,11 @@ function isAdPage() {
     return { "single": single, "list": list, "url": url, "installed": dataInstalled, "result": single || list };
 }
 
-function checkUrlId(willhabenId) {
+function getIdFromUrl() {
     const url = window.location.toString();
     const v = url.split("-");
     const idFromUrl = v[v.length-1].split("/")[0];
-    if (idFromUrl === willhabenId) {
-        return true;
-    } else {
-        console.warn("[willmehrhaben] JSON data ID (" + willhabenId + ") does not match the id from the URL (" + idFromUrl + ")");
-        return false;
-    }
+    return idFromUrl;
 }
 
 function getEmptyPlaceholder() {
@@ -71,23 +74,90 @@ function insertReloadButton(str) {
     }
 }
 
+function getAdJsonData(id) {
+
+    const request = new Request("https://www.willhaben.at/webapi/iad/atverz/" + id + "?formatEnableHtmlTags=true", {
+        headers: {
+            "Accept": "application/json",
+            "X-WH-Client": "api@willhaben.at;responsive_web;server;1.0.0;desktop",
+            "Cache-Control": "no-cache"
+        }
+    });
+    return fetch(request)
+        .then((response) => {
+            return response.json();
+        }).then((jsonData) => {
+            return { "advertDetails" : jsonData };
+        })
+        .catch((e) => {
+            console.error("[willmehrhaben] failed to fetch ad JSON data for ad " + id);
+            return null;
+        });
+}
+
+function getListJsonData() {
+    const path = window.location.pathname.replace("/iad/", "") + window.location.search;
+
+    const request = new Request("https://www.willhaben.at/webapi/iad/search/atz/seo/" + path, {
+        headers: {
+            "Accept": "application/json",
+            "X-WH-Client": "api@willhaben.at;responsive_web;server;1.0.0;desktop",
+            "Cache-Control": "no-cache"
+        }
+    });
+    return fetch(request)
+        .then((response) => {
+            return response.json();
+        }).then((jsonData) => {
+            return { "searchResult" : jsonData };
+        })
+        .catch((e) => {
+            console.error("[willmehrhaben] failed to fetch ad JSON data");
+            return null;
+        });
+}
+
 function getPageProps() {
-    if (NEXT_DATA !== null) return NEXT_DATA;
 
-    const dataTag = document.getElementById("__NEXT_DATA__");
+    if (NEXT_DATA === null) {
+        // if NEXT_DATA is null, the data from the __NEXT_DATA__ tag is probably current
+        // need to switch to using the JSON API
+        const dataTag = document.getElementById("__NEXT_DATA__");
+        if (!dataTag) {
+            console.warn("[willmehrhaben] could not find NEXT_DATA tag");
+            return new Promise((resolve, reject) => reject(null));
+        } else {
 
-    if (!dataTag) {
-        console.warn("[willmehrhaben] could not find NEXT_DATA tag");
-        return null;
-    } else {
+            const data = JSON.parse(dataTag.textContent);
 
-        const data = JSON.parse(dataTag.textContent);
+            const pageProps = data["props"]["pageProps"]
 
-        const pageProps = data["props"]["pageProps"]
+            NEXT_DATA = pageProps;
 
-        NEXT_DATA = pageProps;
+            return new Promise((resolve, reject) => resolve(pageProps));
+        }
+    }
 
-        return pageProps;
+    // We already have data from the NEXT_DATA tag, need to check if it is current
+    const isAdPageResult = isAdPage();
+
+    if (isAdPageResult["single"]) {
+        const urlId = getIdFromUrl();
+        if (typeof NEXT_DATA["advertDetails"] !== 'undefined') {
+            // the data from NEXT_DATA is current, we can use it
+            if (NEXT_DATA["advertDetails"]["id"] ==  urlId) {
+                return new Promise((resolve, reject) => resolve(NEXT_DATA));
+            }
+        }
+        console.debug("[willmehrhaben] getting upstream JSON data");
+        return getAdJsonData(urlId);
+    } else if (isAdPageResult["list"]) {
+        if (NEXT_DATA !== "") {
+            return new Promise((resolve, reject) => resolve(NEXT_DATA));
+        } else {
+            console.debug("[willmehrhaben] getting upstream JSON data");
+            return getListJsonData();
+        }
     }
 }
 
@@ -99,18 +169,14 @@ function injectPublishedDate() {
 
     console.debug("[willmehrhaben] injecting date...");
 
-    const pageProps = getPageProps();
+    getPageProps().then((pageProps) => {
 
-    let appendString = "";
-
-    if (pageProps === null || typeof pageProps === 'undefined') {
-        appendString = " ⚠ ";
-    } else {
+        let appendString = "";
         if (!pageProps["advertDetails"]) {
             console.warn("[willmehrhaben] could not find 'advertDetails' property");
             insertReloadButton();
         } else {
-            if (checkUrlId(pageProps["advertDetails"]["id"])) {
+            if (getIdFromUrl() === pageProps["advertDetails"]["id"]) {
                 const firstPublished = pageProps["advertDetails"]["firstPublishedDate"];
                 appendString = " | Veröffentlicht: " + reformatDate(firstPublished);
                 insertReloadButton(appendString);
@@ -119,12 +185,9 @@ function injectPublishedDate() {
                 insertReloadButton();
             }
         }
-    }
+        console.debug("[willmehrhaben] done");
+    });
 
-    console.debug("[willmehrhaben] done");
-
-    if (pageProps === null || typeof pageProps === 'undefined') {
-    }
 }
 
 function getAttribute(item, attributeName) {
@@ -146,19 +209,17 @@ function getAttribute(item, attributeName) {
 function listPageInsertPubDate() {
     console.debug("[willmehrhaben] injecting dates in list items...");
 
-    const pageProps = getPageProps();
+    getPageProps().then((pageProps) => {
 
-    if (pageProps === null || typeof pageProps === 'undefined') {
-    } else {
         const htmlTag = document.documentElement;
         const scrollY = window.scrollY;
-        htmlTag.scroll({"left":0, "top":2*htmlTag.getClientRects()[0]["height"], "behavior": "smooth"});
+        htmlTag.scroll({"left":0, "top":0.5*document.body.offsetHeight, "behavior": "smooth"});
+        setTimeout(() => htmlTag.scroll({"left":0, "top":document.body.offsetHeight, "behavior": "smooth"}), 200);
 
         const adList = pageProps["searchResult"]["advertSummaryList"]["advertSummary"];
 
         setTimeout(() => {
             adList.forEach((item) => {
-
 
                 const willhabenId = item["id"];
                 const itemATag = document.getElementById("search-result-entry-header-" + willhabenId);
@@ -189,9 +250,12 @@ function listPageInsertPubDate() {
                 }
             });
 
-            htmlTag.scroll(0, scrollY);
+            injectToggleOldButton();
+            document.getElementById("wmh_inject_button").remove();
+
         }, 1000);
-    }
+        setTimeout(() => htmlTag.scroll(0, scrollY), 1000);
+    })
 }
 
 function toggleOldElements() {
@@ -206,10 +270,8 @@ function toggleOldElements() {
         button.textContent = show_old_text;
     }
 
-    const pageProps = getPageProps();
+    getPageProps().then((pageProps) => {
 
-    if (pageProps === null || typeof pageProps === 'undefined') {
-    } else {
         const adList = pageProps["searchResult"]["advertSummaryList"]["advertSummary"];
 
         adList.forEach((item) => {
@@ -231,26 +293,18 @@ function toggleOldElements() {
                 }
             }
         });
-    }
-
+    });
 }
 
 function removeAds() {
     let i = 1;
     document.getElementById("apn-large-leaderboard").parentNode.parentNode.parentNode.classList.add("wmh_hidden");
+    document.querySelector('[data-testid="top-ads-large"]').classList.add("wmh_hidden");
     while(true) {
         const ad = document.getElementById("apn-large-result-list-" + i);
         if (ad === null) return;
         ad.classList.add("wmh_hidden");
         i += 1;
-    }
-}
-
-function wrapperInsertPubDate(isAdPageResult) {
-    if (isAdPageResult["single"]) {
-        injectPublishedDate();
-    } else if (isAdPageResult["list"]) {
-        listPageInsertPubDate();
     }
 }
 
@@ -306,27 +360,45 @@ const attrObserver = new MutationObserver((mutations) => {
     setTimeout( () => {
         if ((mu.attributeName === "class" && mu.target.className === " ") || (mu.attributeName === "data-lt-installed" && mu.target.attributes["data-lt-installed"].value === "true")) {
             console.debug("[willmehrhaben] html has finished loading (" + mu.attributeName + ")");
-            wrapperInsertPubDate(isAdPageResult);
+            if (isAdPageResult["single"]) {
+                injectPublishedDate();
+            }
+            if (isAdPageResult["list"]) {
+                const toRemove = ["wmh_hide_button", "wmh_inject_button"]
+                toRemove.forEach(tryRemove);
+
+                injectButton();
+                removeAds();
+            }
+        } else if (mu.attributeName === "class" && mu.target.className === "nprogress-busy") {
+            console.debug("[willmehrhaben] navigating to another page, clearing NEXT_DATA");
+            NEXT_DATA = "";
         } else {
             console.debug("[willmehrhaben] html is still loading");
         };
-    }, 500);
+    }, 100);
   });
 });
 
-if (checkRealEstate()) {
-    const htmlTag = document.documentElement;
-    attrObserver.observe(htmlTag, {attributes: true});
+checkRealEstate().then((res) => {
+    if (res) {
+        const htmlTag = document.documentElement;
+        console.debug("[willmehrhaben] attaching mutation observer");
+        attrObserver.observe(htmlTag, {attributes: true});
 
-    const isAdPageResult = isAdPage();
-    if (isAdPageResult["result"] && isAdPageResult["installed"]) {
-        console.debug("[willmehrhaben] data already installed, injecting date");
-        setTimeout(() => wrapperInsertPubDate(isAdPageResult), 1000);
+        const isAdPageResult = isAdPage();
+        if (isAdPageResult["single"] && isAdPageResult["installed"]) {
+            console.debug("[willmehrhaben] data already installed, injecting date");
+            injectPublishedDate();
+        } else {
+            console.debug("[willmehrhaben] not an ad page or data not installed");
+        }
+
+        if (isAdPageResult["list"]) {
+            injectButton();
+            removeAds();
+        }
     } else {
-        console.debug("[willmehrhaben] not an ad page or data not installed");
+        console.debug("[willmehrhaben] not on an ad page");
     }
-
-    injectButton();
-    injectToggleOldButton();
-    setTimeout(removeAds, 2000);
-}
+})
